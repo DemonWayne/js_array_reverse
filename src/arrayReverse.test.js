e55f3cf82da591dfec1b25a2690df69b547cdbef
Dmytro <dimusgamer2.0@gmail.com>
2026-01-09 01:29:04 +0200

    implement tests

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,40 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
+    expect(arrayReverse([])).toBeInstanceOf(Array);
+  });
 
+  it(`should return an empty array`, () => {
+    expect(arrayReverse([])).toEqual([]);
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+    expect(arrayReverse([''])).toEqual(['']);
   });
 
-  // write more tests here
+  it('should return the same string if it consists of one character', () => {
+    expect(arrayReverse(['a'])).toEqual(['a']);
+  });
+
+  it(`should return reversed string
+    if the original array consists of one word`, () => {
+    expect(arrayReverse(['Hello'])).toEqual(['olleH']);
+  });
+
+  it(`should return reversed string
+      if original array consists of string with numbers`, () => {
+    expect(arrayReverse(['h311o'])).toEqual(['o113h']);
+  });
+
+  it(`should return reversed string
+      if original array consists of string with special symbols`, () => {
+    expect(arrayReverse(['h@llo!'])).toEqual(['!oll@h']);
+  });
+
+  it(`should return array with reversed words
+      if original array consists of several words`, () => {
+    expect(arrayReverse([`It's not a lake,`, ` `, `it's an ocean`]))
+      .toEqual([`naeco na s'ti ,e`, `k`, `al a ton s'tI`]);
+  });
 });
